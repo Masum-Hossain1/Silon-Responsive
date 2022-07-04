3e99f37a37409e0bfd612c57f3519f44e5627e7d
Masum Hossain <masumhossain957@gmail.com>
2022-07-04 10:45:32 +0600

    Silon Responsive Website Design

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// function myFunction (name, time) {
+//     console.log("My Name is " + name +" I am " + time + " Years Old");
+// }
+// myFunction("Masum", 21);
+// myFunction("Sagor", 18);
+// myFunction("Maidul", 19);
+// myFunction("Shahin", 25);
+
+
+// const car = {
+//     name: "BMW",
+//     model: 500,
+//     weight: "850KG",
+//     start: function () {
+//         console.log("Car Has Start");
+//     },
+//     drive: function () {
+//         console.log("Car is Drive");
+//     },
+// }
+
+// console.log(car.start());
+
+
+
+// function myFunction() {
+//     document.querySelector('#demo').innerHTML = Date();
+// }
+
+
+// const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+// function countVowels(sentence){
+//     let count = 0;
+//     const letters = Array.from(sentence);
+
+//     letters.forEach(function(value){
+//         if(vowels.includes(value)){
+//             count++;
+//         }
+//     });
+
+//     return count;
+
+// }
+// console.log(countVowels("I Love Bangladesh My Name is Masum"));
